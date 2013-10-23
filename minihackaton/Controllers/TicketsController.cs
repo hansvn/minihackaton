ca745284f406dbe8292f2550e5f88329ca948c6a
hansvn <hans_vn@hotmail.com>
2013-10-23 11:05:36 +0200

    added ticket controller and view

---
+++ minihackaton/Controllers/TicketsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace minihackaton.Controllers
+{
+    public class TicketsController : Controller
+    {
+        //
+        // GET: /Tickets/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Create/
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: /Check/
+        public ActionResult Check()
+        {
+            return View();
+        }
+
+    }
+}
