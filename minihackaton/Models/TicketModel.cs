f9c1d79f4d1ed71bac12b97199f0f5812a8f61f4
hansvn <hans_vn@hotmail.com>
2013-10-23 11:24:14 +0200

    added datacontext and some code in the models

---
+++ minihackaton/Models/TicketModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace minihackaton.Models
+{
+    public class TicketModel
+    {
+        private TicketServiceModelDataContextDataContext dc = new TicketServiceModelDataContextDataContext();
+
+        public List<Ticket> selectAllTickets()
+        {
+            var result = (from t in dc.Tickets
+                          select t).ToList<Ticket>();
+            return result;
+        }
+
+        public List<User> selectAllUsers()
+        {
+            var result = (from u in dc.Users
+                          select u).ToList<User>();
+            return result;
+        }
+
+        // INSERT
+        public void insertTicket(Ticket p_ti)
+        {
+            dc.Tickets.InsertOnSubmit(p_ti);
+            dc.SubmitChanges();
+        }
+
+        public void insertUser(User p_us)
+        {
+            dc.Users.InsertOnSubmit(p_us);
+            dc.SubmitChanges();
+        }
+
+        // GET THE USER ID (niet nodig hier)
+        /*public int getUserId(User p_us)
+        {
+            var result = (from e in dc.Users
+                          where e.Lastname == p_us.Lastname
+                          select e.Id).Single();
+            return Convert.ToInt16(result);
+        }*/
+    }
+}
